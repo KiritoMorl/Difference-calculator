c3c79ebceccec7c4e950a8eec543046450c838b2
Kurbonov P <kroritord@bk.ru>
2023-05-02 21:00:37 +0300

    fix index.tests.js

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -6,11 +6,11 @@ const fileExt = ['.json', '.yml'];
 
 const resultStylish = fs.readFileSync(
   path.resolve(process.cwd(), '__fixtures__/result_stylish.txt'),
-  'utf-8'
+  'utf-8',
 );
 const resultPlain = fs.readFileSync(
   path.resolve(process.cwd(), '__fixtures__/result_plain.txt'),
-  'utf-8'
+  'utf-8',
 );
 
 test.each(fileExt)('testing different file options', (extension) => {
