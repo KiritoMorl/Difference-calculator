a50ced9c4ee399bbbbe693f0d2c658e49c68987d
Kurbonov P <kroritord@bk.ru>
2023-05-02 19:03:25 +0300

    change .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { Command } from 'commander/esm.mjs';
 import genDiff from '../src/index.js';
+
 const program = new Command();
 
 program
@@ -9,8 +10,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
   });
 
 program.parse(process.argv);
