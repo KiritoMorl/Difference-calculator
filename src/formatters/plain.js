d2cdbce6bd909a38d973a88a0c838a37f1c853d1
Kurbonov P <kroritord@bk.ru>
2023-05-11 03:32:27 +0300

    fix plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,23 +6,26 @@ const stringify = (value) => {
   }
   return typeof value === 'string' ? `'${value}'` : value;
 };
-
-const getPlainFormat = (value, parent = '') => {
-  switch (value.type) {
-    case 'added':
-      return `Property '${parent}${value.key}' was added with value: ${stringify(value.value)}`;
-    case 'deleted':
-      return `Property '${parent}${value.key}' was removed`;
-    case 'unchanged':
-      return null;
-    case 'changed':
-      return `Property '${parent}${value.key}' was updated. From ${stringify(value.valueBefore)} to ${stringify(value.valueAfter)}`;
-    case 'nested':
-      return value.children.map((val) => getPlainFormat(val, `${parent + value.key}.`))
-        .filter((item) => item !== null).join('\n');
-    default:
-      throw new Error(`Unknown type: ${value.type}`);
-  }
+const plain = (nodes) => {
+  const getPlainFormat = (value, parent = '') => {
+    switch (value.type) {
+      case 'added':
+        return `Property '${parent}${value.key}' was added with value: ${stringify(value.value)}`;
+      case 'deleted':
+        return `Property '${parent}${value.key}' was removed`;
+      case 'unchanged':
+        return null;
+      case 'changed':
+        return `Property '${parent}${value.key}' was updated. From ${stringify(value.valueBefore)} to ${stringify(value.valueAfter)}`;
+      case 'nested':
+        return value.children.map((val) => getPlainFormat(val, `${parent}${value.key}.`))
+          .filter((item) => item !== null).join('\n');
+      default:
+        throw new Error(`Unknown type: ${value.type}`);
+    }
+  };
+  const diff = nodes.map((element) => getPlainFormat(element));
+  const result = diff.filter(Boolean);
+  return `${result.join('\n')}`;
 };
-
-export default (plain) => `${plain.map((element) => getPlainFormat(element)).join('\n')}`;
+export default plain;
